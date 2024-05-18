d7188b1254a0108a8af051d68b796e2a79605041
Pilar Fernandez Nieto <rla10309@educastur.es>
2024-05-18 18:55:08 +0200

    Sube productos a firebase

---
+++ src/stores/products.js
@@ -0,0 +1,36 @@
+import { computed } from "vue";
+import { defineStore} from "pinia";
+import { useFirestore } from "vuefire";
+import { collection, addDoc } from "firebase/firestore";
+
+export const useProductsStore = defineStore("products", () => {
+
+
+    const db = useFirestore();
+    const categories = [
+        {id: 1, name: "Sudaderas"},
+        {id: 2, name: "Tenis"},
+        {id: 3, name: "Lentes"}
+    ]
+    async function createProduct(product){
+       await addDoc(collection(db, "products"), product)
+    }
+
+    // Options del select de categorias
+    const categoryOptions = computed(() => {
+        const options = [
+            {label: "Seleccione", value: "", attrs: {disabled: true}},
+            ...categories.map(category => (
+                {label: category.name, value: category.id}
+            ))
+        ]
+        return options
+    })
+
+
+
+    return {
+        createProduct,
+        categoryOptions
+    }
+})
