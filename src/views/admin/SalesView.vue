c5984a4ffacd66c7b3f50231e78e15a414326f33
Pilar Fernandez Nieto <rla10309@educastur.es>
2024-05-26 13:41:19 +0200

    ajustes finales

--- src/views/admin/SalesView.vue
+++ src/views/admin/SalesView.vue
@@ -18,7 +18,7 @@ console.log(sales.salesCollection);
 <template>
     <h1 class="text-4xl font-black my-10">Resumen de ventas</h1>
     <div class="md:flex md:items-start gap-5">
-        <div class="lg:w-1/2 xl:w-1/3 bg-white justify-center p-5">
+        <div class="lg:w-1/2 xl:w-1/3 bg-white flex justify-center p-5">
             <VueTailwindDatepicker v-model="sales.dateValue" i18n="es" as-single no-input :formatter="formatter" />
         </div>
         <div class="md:w-1/2 lg:w-2/3 space-y-5 lg:h-screen lg:overflow-y-scroll p-5 pb-32">
@@ -27,7 +27,7 @@ console.log(sales.salesCollection);
             <p v-else class="text-center text-lg">Selecciona una fecha</p>
 
             <div v-if="sales.salesCollection.length" class="space-y-5">
-                <p class="text-right text-2xl">Total del día: <span class="font-black">{{
+                <p class="text-right text-2xl bg-indigo-200 p-2">Total del día: <span class="font-black">{{
                     formatCurrency(sales.totalSalesOfDay) }}</span></p>
                 <SaleDetails v-for="sale in sales.salesCollection" :key="sale.id" :sale="sale" />
             </div>
